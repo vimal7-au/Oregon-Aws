8a9deae29a714b75cd269dd5bf6bc4fc3a367e28
VRadh1 <vimal.radhakrishnanmaliakkal@nike.com>
2018-06-12 21:26:13 +1000

    Initial commit

---
+++ src/main/java/com/nike/aws/lambda/EventHandler.java
@@ -0,0 +1,14 @@
+package com.nike.aws.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class EventHandler implements RequestHandler<Object, Object> {
+
+    @Override
+    public String handleRequest(Object input, Context context) {
+        System.out.println("Handle request invokedddd");
+        return "Received message: " + input;
+    }
+
+}
